dc7898f5ab2a8ce7b293d1f86b2b577d590b4b49
jiteshm17 <33426178+jiteshm17@users.noreply.github.com>
2018-10-31 09:10:54 +0530

    Created customer and vendor apps

---
+++ ASE1/customer/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from customer.models import CustomerProfile
+
+# Register your models here.
+
+admin.site.register(CustomerProfile)
+
---
+++ ASE1/vendor/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from vendor import views
+
+app_name = 'vendor'
+
+urlpatterns = [
+    path('add/', views.add_products, name='add_products'),
+    path('view/', views.view_products, name='view_products'),
+    path('modify/<int:id>/', views.modify_products, name='modify_products'),
+    path('login/', views.vendor_login, name='login'),
+    path('logout/', views.vendor_logout, name='logout'),
+    path('signup/', views.vendor_signup, name='signup')
+]
