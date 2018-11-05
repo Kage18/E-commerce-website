e9eb5d800b4a60a523a0a459f13841da778bb9cc
jiteshm17 <33426178+jiteshm17@users.noreply.github.com>
2018-11-05 22:14:11 +0530

    recovered cache

---
+++ ASE1/customer/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from customer import views
+
+app_name = 'customer'
+
+urlpatterns = [
+    path('home/', views.IndexView.as_view(), name='home'),
+    path('home/<int:pk>/', views.ItemsView.as_view(), name='items'),
+    path('login/', views.customer_login, name='login'),
+    path('logout/', views.customer_logout, name='logout'),
+    path('signup/', views.customer_signup, name='signup'),
+]
