f0eb93332b1f36738a0c64f29cc842077c99e656
Kage18 <18kushaljain@gmail.com>
2018-12-08 17:39:59 +0530

    Deleted all files

--- ASE1/actor_authentication/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from customer.models import CustomerProfile
-from vendor.models import VendorProfile
-# Create your models here.
--- ASE1/cart/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from cart.models import Order, OrderItem, Transaction
-
-admin.site.register((Order, OrderItem, Transaction))
